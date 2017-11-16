fd3d06983238fb8a973ed946b6b6a3813d5c8d4e
Jim Tyhurst <jim@tyhurst.com>
2017-11-16 10:36:37 -0800

    Added histogram of tweet lengths.

--- R/explore.R
+++ R/explore.R
@@ -13,10 +13,23 @@ explore <- function() {
     dplyr::group_by(class) %>%
     dplyr::summarize(total = n()) %>%
     ggplot(aes(x = factor(class), y = total)) +
-    geom_bar(stat = "identity", color = "blue", fill = "blue") +
+    geom_bar(stat = "identity") +
     geom_text(aes(label = total, vjust = -0.25)) +
     scale_x_discrete(labels = c("hate speech", "offensive language", "neither")) +
     xlab("class") +
     ylab("quantity") +
-    ggtitle("Number of tweets in each class")
+    ggtitle("Number of tweets in each class (data: twitter-hate-speech2.csv)") %>% 
+    print()
+  
+  # Histogram of tweet lengths
+  encoded_tweets %>% 
+    mutate(tweet_length = nchar(tweet)) %>% 
+    ggplot(aes(x = tweet_length)) +
+    geom_histogram(binwidth = 1) +
+    scale_x_continuous(name = "Number of characters in tweet", 
+                       limits = c(0, 300), 
+                       expand = c(0, 0)) +
+    ylab("Number of tweets") +
+    ggtitle("Length of tweets (data: twitter-hate-speech2.csv)") %>% 
+    print()
 }
