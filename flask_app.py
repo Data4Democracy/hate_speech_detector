61c46b4f1103a1212ace504296c51d6dd840f9f2
Julius Simonelli <julius.simonelli@gmail.com>
2017-12-12 02:07:31 +0000

    Add initial flask app file, update .gitignore

---
+++ flask_app.py
@@ -0,0 +1,11 @@
+
+# A very simple Flask Hello World app for you to get started with...
+
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return 'Hello from Flask!'
+
